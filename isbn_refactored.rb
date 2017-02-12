a661766a52175127516239e32c4637ea11b75657
edwinwells <edwin.wallace@yandex.com>
2017-02-12 15:04:26 -0500

    Starting from the beginning again; either tests were failing & program running, or tests passing & program not functioning.  I will utilize some ideas I came up with during my experimentation, first being two functions finalizing whether or no a # is valid.

--- isbn_refactored.rb
+++ isbn_refactored.rb
@@ -1,96 +1,91 @@
 def remove_unwanted(isbn_string)
-# Step 1: This will create a new array, named 'refreshed', to be manipulated throughout the program:
+# Step 1: This will create a new array, named 'refreshed', without any char that's not an integer or an 'x':
 ## Step 2: then it will call function "check_count", ensuring there are only 10 characters present:
-	puts "Original number is:  #{isbn_string}\n"	
-	refreshed=isbn_string.split("")
-	puts "refreshed is:  #{refreshed}\n"
 
+	clay = isbn_string.split("")
+	refreshed=[]
 
-	if refreshed.length==10
-		for i in 0..8 do
-				if refreshed[i] == "x" || refreshed[i] == "X"
-				  invalid=true
-				end
+	clay.each do |filter|
+
+		if  filter == "0" || filter == "1" || filter == "2" || filter == "3" || filter == "4" || filter == "5" || filter == "6" || filter == "7" || filter == "8" || filter == "9" || filter =="X" || filter =="x"
+
+			refreshed.push(filter)
 		end
-		return false if invalid==true
-		invalid_number(refreshed) if invalid==true
-		check_the_sum(refreshed) if invalid!=true
+	end
+
+	check_count(refreshed)
+end
+
+
+def check_count(refreshed)
+
+	if refreshed.length<10
+		puts "\nIt's less than 10 digits!\n"
+		false
+
+	elsif refreshed.length>10
+		puts "\nIt's more than 10 digits!\n"
+		false
+
+	elsif refreshed.length==10
+		puts "\nIt's 10 digits!\n"
+		true
+		# check_x(refreshed)
 	end
 end
 
+
 def check_x(refreshed)
 
+	for i in 0..8 do
+		if refreshed[i] == "x" || refreshed[i] == "X"
+			puts "Invalid ISBN !!!"
+			return false
+		end
+	end
 
-	true
-	puts "Heading to check_the_sum(refreshed)!"
-	check_the_sum(refreshed)
+	check_count(refreshed)
 end
 
 
 def check_the_sum(refreshed)
 # First, identify the user-given check-number:
+
 user_given_check_number=refreshed[9]
-puts user_given_check_number
+
 	sum=0
 	x=1
+
 	for n in (0..8) do
 		refreshed[n]=refreshed[n].to_i
 		addnum=refreshed[n]*x
 		sum+=addnum
 		x+=1
+
 	end
 
 # semifinal is the check-number:
 semifinal=sum%11
-puts semifinal
+
 # Now, verify that the calculated check-number is valid:
+refreshed[9]=refreshed[9].to_i
 
-	if  user_given_check_number.to_i==semifinal
+	if  refreshed[9]==semifinal
+		puts "\nCongratulations!\nYour ISBN #{refreshed.join("")} is Valid!\nHave a nice day:-)\n"
 		true
-		valid_number(refreshed)
-
 	elsif user_given_check_number=="x" && semifinal==10
+		puts "\nCongratulations!\nYour ISBN #{refreshed[0..8].join("")}x is Valid!\nHave a nice day:-)\n"
 		true
-		valid_number(refreshed)
-
 	 elsif user_given_check_number=="X" && semifinal==10
-		true
-		valid_number(refreshed)
-
+		puts "\nCongratulations!\nYour ISBN #{refreshed[0..8].join("")}X is Valid!\nHave a nice day:-)\n"
 	else
+		puts "\nSorry 'bout your luck!\nYou got hold of a counterfeit ISBN!\nBetter luck next time...\n\n"
 		false
-		invalid_number(refreshed)
 	end
 end
-
-
-def invalid_number(refreshed)
-		false
-		puts "\nSorry 'bout your luck!\nYou got hold of a counterfeit ISBN!\nBetter luck next time...\n\n"
-end
-
-def valid_number(refreshed)
-		puts "\nCongratulations!\nYour ISBN #{refreshed.join("")} is Valid!\nHave a nice day:-)\n\n"
-end
-
-def check_the_sum_other_than_ten_chars(refreshed)
-
-
-end
-
-def function_carpool
-
-end
-
-
-
-
 
 
 # remove_unwanted("0-66586*7x84") #INVALID
 # remove_unwanted("7421394761") #VALID
 # remove_unwanted("7421394765") #INVALID
-# remove_unwanted("0471958697") #VALID
-remove_unwanted("877195869x") #VALID
-# remove_unwanted("877195x869") #INVALID
-
+# print remove_unwanted("7195869X") #VALID
