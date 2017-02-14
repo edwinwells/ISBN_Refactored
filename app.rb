5a75c3e39342abe44f1c5068ff2bbd3dffc9e504
edwinwells <edwin.wallace@yandex.com>
2017-02-14 12:32:05 -0500

    Trying to understand the calls. Input section functions, nothing returning. Don't understand the 'post' command...

--- app.rb
+++ app.rb
@@ -1,8 +1,26 @@
+# require 'sinatra'
+
+# get '/' do
+# 	# 'Hello world!'
+# 	erb :User_Input_ISBN_Number
+# end
+
+# post
+
+
 require 'sinatra'
+require_relative 'isbn_refactored.rb'
 
 get '/' do
-	# 'Hello world!'
-	erb :User_Input_ISBN_Number
+    erb :User_Input_ISBN_Number
 end
 
-post
+post '/remove_unwanted(isbn_string)' do
+# result = remove_unwanted(isbn_num)
+# 	'#{result}'
+end
+
+# post '/isbn_refactored' do
+#     # result = remove_unwanted(isbn_num)
+#     # "#{result}"
+# end
